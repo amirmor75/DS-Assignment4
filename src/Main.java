1a2601e016b0ef23ed550c2de67e3edc588e83fd
amirmor75 <amirmor75@gmail.com>
2019-06-02 20:25:24 +0300

    main deleted

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BTree tree =new BTree("2");
-        tree.createFullTree("C:\\Users\\amirm_000\\Desktop\\Repositories\\DS-Assignment4\\requested_passwords.txt");
-        System.out.println(tree);
-    }
-}
